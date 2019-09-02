8291dace7a41860691ce4c9751f39576fc6c3576
Drew Moseley <drew.moseley@northern.tech>
2019-09-02 14:39:09 -0400

    Tweak X_MIN_POS
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -883,7 +883,7 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -38
+#define X_MIN_POS -42
 #define Y_MIN_POS -13
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
