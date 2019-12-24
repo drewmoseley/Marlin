df60220655fa444702785f05bac31d10a83f64d7
Drew Moseley <drew.moseley@northern.tech>
2019-12-23 20:02:59 -0500

    Tweak E Steps
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -621,7 +621,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 101.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 103.0 }
 
 /**
  * Default Max Feed Rate (mm/s)
