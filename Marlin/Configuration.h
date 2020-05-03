2c171502ef4a6a07117bb9a4aa6219bab32da056
Drew Moseley <drew.moseley@northern.tech>
2020-05-03 14:44:29 -0400

    PID for Microswiss HotEnd

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -483,9 +483,9 @@
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
 
   // Creality Ender-3 - M303 C5 E0 S200 U1
-  #define DEFAULT_Kp 31.72
-  #define DEFAULT_Ki 3.38
-  #define DEFAULT_Kd 74.38
+  #define DEFAULT_Kp 36.26
+  #define DEFAULT_Ki 5.38
+  #define DEFAULT_Kd 61.05
 
 #endif // PIDTEMP
 
