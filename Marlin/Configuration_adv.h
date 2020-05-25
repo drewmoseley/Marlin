7bb42ea7d69c8bb3f9e92b91e485c0eaa74d91b5
Drew Moseley <drew.moseley@northern.tech>
2020-05-25 15:31:54 -0400

    Disable LIN_ADVANCE.
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1538,7 +1538,7 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
-#define LIN_ADVANCE
+//#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 0.95    // Unit: mm compression per 1mm/s extruder speed
