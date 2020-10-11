4adcdf9e49471e4ddbc6f6dcdabbe903e8fbee3d
Drew Moseley <drew.moseley@northern.tech>
2020-10-11 17:32:52 -0400

    Tweak for Microswiss reinstall
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -488,9 +488,9 @@
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
 
   // Creality Ender-3 - M303 C5 E0 S200 U1
-  #define DEFAULT_Kp 25.17
-  #define DEFAULT_Ki 2.22
-  #define DEFAULT_Kd 71.28
+  #define DEFAULT_Kp 23.39
+  #define DEFAULT_Ki 2.15
+  #define DEFAULT_Kd 63.49
 #endif // PIDTEMP
 
 //===========================================================================
@@ -531,9 +531,9 @@
   //#define DEFAULT_bedKd 230.70
 
   //Ender 3: M303 C8 E-1 S60 U1
-  #define DEFAULT_bedKp 100.19
-  #define DEFAULT_bedKi 18.07
-  #define DEFAULT_bedKd 370.41
+  #define DEFAULT_bedKp 106.53
+  #define DEFAULT_bedKi 20.91
+  #define DEFAULT_bedKd 361.90
 #endif // PIDTEMPBED
 
 #if EITHER(PIDTEMP, PIDTEMPBED)
@@ -975,7 +975,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -7, -2.45 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -7, -2.55 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
