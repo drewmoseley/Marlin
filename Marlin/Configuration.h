420c050f7cb384ac88d653a43027a0bc85d5bcc9
Drew Moseley <drew.moseley@northern.tech>
2020-11-27 14:36:18 -0500

    Tweak for new fan duct
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1000,11 +1000,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 28, 6, -0.83 }
+#define NOZZLE_TO_PROBE_OFFSET { 38, 6, -0.8 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 15
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED 12000
