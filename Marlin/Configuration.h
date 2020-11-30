3174eb38fa25b30b49abfd252a52107e01f6191a
Drew Moseley <drew.moseley@northern.tech>
2020-11-30 13:46:53 -0500

    Adjust bed size and zero position.
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1111,12 +1111,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 233
+#define X_BED_SIZE 235
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -5
+#define Y_MIN_POS -13
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
