3aea204ce0337919d4a33d7fd17b9245f99793df
Drew Moseley <drew@moseleynet.net>
2021-03-20 12:03:51 -0400

    Tweaks for new X Carriage
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -997,7 +997,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38, 0, -0.7 }
+#define NOZZLE_TO_PROBE_OFFSET { -31, 0, -0.7 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1133,12 +1133,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 210
+#define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -22
-#define Y_MIN_POS -9
+#define X_MIN_POS 0
+#define Y_MIN_POS -17
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
