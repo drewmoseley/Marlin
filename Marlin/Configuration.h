678e2dc7e8ea3f7cb4f5f1ab84e199df5c227662
Drew Moseley <drew@moseleynet.net>
2021-05-30 17:23:14 -0400

    Revert "Tweaks for new X Carriage"
    This reverts commit 3aea204ce0337919d4a33d7fd17b9245f99793df.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -997,7 +997,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -31, 0, -1.65 }
+#define NOZZLE_TO_PROBE_OFFSET { 18, 66, -0.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1133,12 +1133,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 210
-#define Y_BED_SIZE 210
+#define X_BED_SIZE 200
+#define Y_BED_SIZE 190
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS -17
+#define X_MIN_POS -38
+#define Y_MIN_POS -11
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
