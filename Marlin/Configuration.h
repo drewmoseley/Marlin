714b84aa7602bf52f31af578c46490cecbfe76c0
Drew Moseley <drew@moseleynet.net>
2021-09-09 20:18:15 -0400

    Calibrate
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -504,9 +504,9 @@
   #else
     // ANET A8 Custom E3DV6 Extruder at 210 Degree Celsius and 100% Fan
     // Drew: "M303 C8 E0 S205 U1"
-    #define DEFAULT_Kp 22.82
-    #define DEFAULT_Ki 1.19
-    #define DEFAULT_Kd 108.91
+    #define DEFAULT_Kp 18.45
+    #define DEFAULT_Ki 1.06
+    #define DEFAULT_Kd 80.49
   #endif
 #endif // PIDTEMP
 
@@ -552,9 +552,9 @@
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
   // ANET A8 Custom
   // Drew: "M303 C8 E-1 S60 U1"
-  #define DEFAULT_bedKp 110.48
-  #define DEFAULT_bedKi 21.14
-  #define DEFAULT_bedKd 384.82
+  #define DEFAULT_bedKp 124.07
+  #define DEFAULT_bedKi 24.79
+  #define DEFAULT_bedKd 413.99
 #endif // PIDTEMPBED
 
 #if EITHER(PIDTEMP, PIDTEMPBED)
@@ -996,7 +996,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 36, 0, -1.15 }
+#define NOZZLE_TO_PROBE_OFFSET { 36, 0, -1.35 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
