f039c11c2864c4ddc1c0dbf7cf63a51bd37fb076
Drew Moseley <drew@moseleynet.net>
2023-10-22 17:15:33 -0400

    Enable and configure skew compensation
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1793,31 +1793,31 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-//#define SKEW_CORRECTION
+#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
-  #define XY_DIAG_AC 282.8427124746
-  #define XY_DIAG_BD 282.8427124746
-  #define XY_SIDE_AD 200
+  #define XY_DIAG_AC 141.6264
+  #define XY_DIAG_BD 142.2022
+  #define XY_SIDE_AD 99.577
 
   // Or, set the default skew factors directly here
   // to override the above measurements:
-  #define XY_SKEW_FACTOR 0.0
+  #define XY_SKEW_FACTOR -0.00411162
 
-  //#define SKEW_CORRECTION_FOR_Z
+  #define SKEW_CORRECTION_FOR_Z
   #if ENABLED(SKEW_CORRECTION_FOR_Z)
-    #define XZ_DIAG_AC 282.8427124746
-    #define XZ_DIAG_BD 282.8427124746
-    #define YZ_DIAG_AC 282.8427124746
-    #define YZ_DIAG_BD 282.8427124746
-    #define YZ_SIDE_AD 200
-    #define XZ_SKEW_FACTOR 0.0
-    #define YZ_SKEW_FACTOR 0.0
+    #define XZ_DIAG_AC 140.721399515501
+    #define XZ_DIAG_BD 142.008900668289
+    #define YZ_DIAG_AC 142.992119280012
+    #define YZ_DIAG_BD 141.914605786505
+    #define XY_SKEW_FACTOR -0.00411162
+    #define XZ_SKEW_FACTOR -0.00906714
+    #define YZ_SKEW_FACTOR 0.0076086
   #endif
 
   // Enable this option for M852 to set skew at runtime
-  //#define SKEW_CORRECTION_GCODE
+  #define SKEW_CORRECTION_GCODE
 #endif
 
 //=============================================================================
