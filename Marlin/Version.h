b18d3188135628fa39a5564ce0410c12f13e15e0
Drew Moseley <drew@moseleynet.net>
2023-10-26 16:10:12 -0400

    Custom version string
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "CR6Comm-Rel6.1"
+#define SHORT_BUILD_VERSION "CR6Comm-Rel6.1-dmoseley"
 
 /**
  * Verbose version identifier which should contain a reference to the location
