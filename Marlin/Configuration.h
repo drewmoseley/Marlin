bff3380e24e5a335c69ccb6f885977c8bbbcb646
Drew Moseley <drew@moseleynet.net>
2024-01-13 17:00:08 -0500

    Adjust bed size back to stock
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1696,12 +1696,12 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 228
-#define Y_BED_SIZE 223
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -2
-#define Y_MIN_POS -14
+#define X_MIN_POS -6
+#define Y_MIN_POS -18
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
