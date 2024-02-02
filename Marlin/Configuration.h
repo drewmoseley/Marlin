d051a01b869b19ad82944ec4c02a805d8457f2a8
Drew Moseley <drew@moseleynet.net>
2024-02-02 18:55:26 -0500

    Reduce build size a bit and move closer to center of bed
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1757,16 +1757,16 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 302
-#define Y_BED_SIZE 302
+#define X_BED_SIZE 295
+#define Y_BED_SIZE 295
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -62
-#define Y_MIN_POS 0
+#define X_MIN_POS -64
+#define Y_MIN_POS -2
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 400
+#define Z_MAX_POS 390
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
