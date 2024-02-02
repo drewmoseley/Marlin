cd2e0a1e404c4883b923ffc5ce277e03052fecaf
Drew Moseley <drew@moseleynet.net>
2024-02-01 22:26:09 -0500

    Switch back to standard steps-per-unit values.
    We will try the new Califlower and adjust in the slicer instead.
    I think...
    
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -805,7 +805,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.8629, 80.1287, 398.7770, 411}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 411}
 
 /**
  * Default Max Feed Rate (mm/s)
